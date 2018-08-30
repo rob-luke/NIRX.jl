e871cf3606138e387dffc41f254da4927de06ad3
Robert Luke <748691+rob-luke@users.noreply.github.com>
2018-08-30 14:00:10 +1000

    Use DataDeps in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,13 @@
 using Test
 using NIRX
+using DataDeps
 
-if ~isdir("data")
-        @info "Downloading data"
-        download("https://s3.amazonaws.com/test.robertluke.net/fNIRS-test-data.zip", "data.zip")
-        run(`unzip data -d data`)
-else
-        @info "Data already exists"
-end
+register(DataDep("NIRX.jl single test file 1", "Single fNIRS experiment recording",
+    ["https://s3.amazonaws.com/test.robertluke.net/fNIRS-test-data.zip"];
+    post_fetch_method = [file->run(`unzip $file`)]
+))
 
-triggers, header_info, info, wl1, wl2, config = read_NIRX("data/fNIRS-test-data")
+triggers, header_info, info, wl1, wl2, config = read_NIRX(string(datadep"NIRX.jl single test file 1", "/fNIRS-test-data"))
 
 @test size(triggers) == (62, 2)
 @test length(header_info) == 22
