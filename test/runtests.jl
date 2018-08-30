36785d3440c2e5bf85326b888f87740bc036b36c
rob-luke <hello@robertluke.net>
2018-08-30 15:45:37 +1000

    Check wavelength values against truth

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,24 @@
 using Test
 using NIRX
 using DataDeps
+using HDF5
 
-register(DataDep("NIRX.jl single test file 1", "Single fNIRS experiment recording",
+## Register useful data dependencies
+
+register(DataDep("NIRX test file 1", "Single fNIRS experiment recording",
     ["https://s3.amazonaws.com/test.robertluke.net/fNIRS-test-data.zip"];
     post_fetch_method = [file->run(`unzip $file`)]
 ))
 
-triggers, header_info, info, wl1, wl2, config = read_NIRX(string(datadep"NIRX.jl single test file 1", "/fNIRS-test-data"))
+register(DataDep("NIRX results file 1", "Single fNIRS experiment recording",
+    ["https://s3.amazonaws.com/test.robertluke.net/fNIRS-test-data.h5.zip"];
+    post_fetch_method = [file->run(`unzip $file`)]
+))
+
+
+## Read example NIRX file and test basic sizes of what is returned
+
+triggers, header_info, info, wl1, wl2, config = read_NIRX(string(datadep"NIRX test file 1", "/fNIRS-test-data"))
 
 @test size(triggers) == (62, 2)
 @test length(header_info) == 23
@@ -15,3 +26,13 @@ triggers, header_info, info, wl1, wl2, config = read_NIRX(string(datadep"NIRX.jl
 @test size(wl1) == (7008, 256)
 @test size(wl2) == (7008, 256)
 @test length(config) == 10
+
+
+# Read known results for wavelengths 1 and 2 and check values
+
+wl1_true = h5read(string(datadep"NIRX results file 1", "/fNIRS-test-data.h5"), "wl1")
+wl2_true = h5read(string(datadep"NIRX results file 1", "/fNIRS-test-data.h5"), "wl2")
+
+@test wl1[:, Bool.(header_info["SourceDetectorMask"][:, 4])] == wl1_true
+@test wl2[:, Bool.(header_info["SourceDetectorMask"][:, 4])] == wl2_true
+
